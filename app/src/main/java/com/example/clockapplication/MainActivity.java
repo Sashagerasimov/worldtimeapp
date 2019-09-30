0845ceee63b2e310fe7c2db544e9106c8a145c50
Sasha G <sasha@oversigma.com>
2019-09-30 23:27:24 +1000

    hide cities

--- app/src/main/java/com/example/clockapplication/MainActivity.java
+++ app/src/main/java/com/example/clockapplication/MainActivity.java
@@ -3,10 +3,11 @@ package com.example.clockapplication;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
-import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.TextClock;
 import android.widget.TextView;
@@ -23,13 +24,100 @@ public class MainActivity extends AppCompatActivity {
     public TextClock textClock;
     public Button button1;
     public Button button2;
-
+    public CheckBox hide;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
+
+       //topbar work
+
+        hide = findViewById(R.id.hideCity1);
+        hide.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    city1.setVisibility(View.VISIBLE);
+                } else {
+                    city1.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        hide = findViewById(R.id.hideCity2);
+        hide.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    city2.setVisibility(View.VISIBLE);
+                } else {
+                    city2.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        hide = findViewById(R.id.hideCity3);
+        hide.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    city3.setVisibility(View.VISIBLE);
+                } else {
+                    city3.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        hide = findViewById(R.id.hideCity4);
+        hide.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    city4.setVisibility(View.VISIBLE);
+                } else {
+                    city4.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        hide = findViewById(R.id.hideCity5);
+        hide.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    city5.setVisibility(View.VISIBLE);
+                } else {
+                    city5.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        hide = findViewById(R.id.hideCity5);
+        hide.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    city5.setVisibility(View.VISIBLE);
+                } else {
+                    city5.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        hide = findViewById(R.id.hideCity6);
+        hide.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    city6.setVisibility(View.VISIBLE);
+                } else {
+                    city6.setVisibility(View.GONE);
+                }
+            }
+        });
+
 
        //buttons to toggle 12/24, note both just modify 12hour format. i.e kk vs h
 
@@ -48,6 +136,8 @@ public class MainActivity extends AppCompatActivity {
                 textClock = city5.findViewById(R.id.timeNow);
                 textClock.setFormat12Hour("kk:mm:ss, E");
                 textClock = city6.findViewById(R.id.timeNow);
+                textClock.setFormat12Hour("kk:mm:ss, E");
+                textClock = findViewById(R.id.timeLocal);
                 textClock.setFormat12Hour("kk:mm:ss, E");
 
                 button1.setBackgroundColor(getColor(R.color.colorPrimaryDark));
@@ -71,6 +161,8 @@ public class MainActivity extends AppCompatActivity {
                 textClock.setFormat12Hour("h:mm:ss a, E");
                 textClock = city6.findViewById(R.id.timeNow);
                 textClock.setFormat12Hour("h:mm:ss a, E");
+                textClock = findViewById(R.id.timeLocal);
+                textClock.setFormat12Hour("h:mm:ss a, E");
 
                 button2.setBackgroundColor(getColor(R.color.colorPrimaryDark));
                 button1.setBackgroundColor(getColor(R.color.colorPrimary));
@@ -81,6 +173,8 @@ public class MainActivity extends AppCompatActivity {
 
         //Paris
         city1 = findViewById(R.id.one);
+
+        //city1.setVisibility(View.GONE);
 
         cityName = city1.findViewById(R.id.loaction);
         cityName.setText(getString(R.string.city1));
